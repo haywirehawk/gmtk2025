578003b6fe9748219250553e1467f80eac76ff55
Mathew Humphrey <deathlysamurai@live.com>
2025-08-01 17:38:18 -0500

    Add functionality for tornado to move in and out

--- game_objects/enemy/tornado/tornado.gd
+++ game_objects/enemy/tornado/tornado.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 
 @export var speed: float = 10
+@export var spread_speed: float = 50
 @export var movement_damping: float = 5.0
 
-@onready var movement_timer: Timer
+@onready var left_side: TornadoSprite = $LeftSprite2D
+@onready var right_side: TornadoSprite = $RightSprite2D
 
+
+var movement_timer: Timer
 var direction: int = 1
 
 
@@ -17,7 +21,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var target_velocity = speed * direction
 	velocity.x = lerp(velocity.x, target_velocity, 1 - exp(-movement_damping * delta))
 	move_and_slide()
@@ -25,3 +29,18 @@ func _process(delta: float) -> void:
 
 func _update_direction() -> void:
 	direction = randi_range(-1, 1)
+	var move_sides_closer = randi_range(-1, 1)
+	if move_sides_closer == 1:
+		_move_sides_closer()
+	elif move_sides_closer == -1:
+		_move_sides_farther()
+
+
+func _move_sides_closer() -> void:
+	left_side.move(spread_speed, 1)
+	right_side.move(spread_speed, -1)
+#
+#
+func _move_sides_farther() -> void:
+	left_side.move(spread_speed, -1)
+	right_side.move(spread_speed, 1)
---
+++ game_objects/enemy/tornado/tornado_sprite_2d.gd
@@ -0,0 +1,16 @@
+class_name TornadoSprite
+extends Sprite2D
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func move(speed: float, direction: int) -> void:
+	var target_velocity = speed * direction
+	var tween := create_tween()
+	tween.tween_property(self, "position", Vector2(position.x + target_velocity, position.y), 1.0)
