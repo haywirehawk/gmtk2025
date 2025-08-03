9bc3c2dea923435087afc23af68e25b51b6aa0c0
Mathew Humphrey <deathlysamurai@live.com>
2025-08-02 21:23:21 -0500

    Make ice block slowly disappear when hit

--- game_objects/environment/poi/ice_block.gd
+++ game_objects/environment/poi/ice_block.gd
@@ -8,5 +8,7 @@ func _ready() -> void:
 	area_2d.body_entered.connect(_melt_ice)
 
 
-func _melt_ice(body: Node2D) -> void:
+func _melt_ice(_body: Node2D) -> void:
+	var tween = create_tween()
+	await tween.tween_property(self, "modulate:a", 0, 1).finished
 	self.queue_free()
