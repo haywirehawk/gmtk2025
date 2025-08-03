04716c56c233d55298b6c749b01e138faaec8b81
Shane <enahsnalob@gmail.com>
2025-08-03 12:53:02 -0500

    asdfasdf

--- game_objects/NPCs/npc_template.gd
+++ game_objects/NPCs/npc_template.gd
@@ -5,17 +5,17 @@ var isInTrigger = false
 var dialogCurrentIndex = 0;
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		$TextHolder.visible = true
-		dialogCurrentIndex = 0
-		$TextHolder/RichTextLabel.text = dialogArray[dialogCurrentIndex]
-		isInTrigger = true
+	print("npc test")
+	$TextHolder.visible = true
+	dialogCurrentIndex = 0
+	$TextHolder/RichTextLabel.text = dialogArray[dialogCurrentIndex]
+	isInTrigger = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		$TextHolder.visible = false
-		isInTrigger = false
+	print("npc test")
+	$TextHolder.visible = false
+	isInTrigger = false
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Interact") && isInTrigger:
