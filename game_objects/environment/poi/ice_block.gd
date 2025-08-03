950edca7f99518ca4c97cb0564149d66f26ef2b8
Mathew Humphrey <deathlysamurai@live.com>
2025-08-02 21:15:02 -0500

    Add ability for ice block to melt from fire lasso

--- game_objects/environment/poi/ice_block.gd
+++ game_objects/environment/poi/ice_block.gd
@@ -1,1 +1,12 @@
 extends Sprite2D
+
+
+@onready var area_2d: Area2D = $Area2D
+
+
+func _ready() -> void:
+	area_2d.body_entered.connect(_melt_ice)
+
+
+func _melt_ice(body: Node2D) -> void:
+	self.queue_free()
